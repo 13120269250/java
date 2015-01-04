f4d191d482c8efdecb9844fc1bb6edeb6b5a90e5
zhujohnle <545948101@qq.com>
2015-01-04 10:56:46 +0800

    add the package of singlepointlist

---
+++ src/singlepointlist/SinglePointLinkedList.java
@@ -0,0 +1,24 @@
+package singlepointlist;
+
+public class SinglePointLinkedList {
+	
+	public int size  = 0;
+	
+	
+    class Element{
+    	String name;
+    	Element next;
+    }
+    
+    public void add(Element mElement){
+    	
+    }
+    
+    public void remove(){
+    	
+    }
+    
+    public Element getElement(int i){
+    	return null;
+    }
+}
