748b681ba58f6d075b2d019636979e2449903877
zhujohnle <545948101@qq.com>
2015-01-04 15:42:55 +0800

    add the test struct

---
+++ src/OrBecome.java
@@ -0,0 +1,24 @@
+
+public class OrBecome {
+	
+	
+	/**
+	 * 
+	 * 亦或运算
+	 * 
+	 * 所有的数据亦或偶数次 结果为0
+	 * 
+	 * 亦或基数次 为 本身
+	 * 
+	 * 
+	 * 若 有一个数组 所有的成员 都是出现偶数次  只有一个成员出现基数次 怎样能快速找到这个出现基数次的数
+	 * 
+	 * 怎么快速计算2*2*2的值 z³ 则向左移动2位即可
+	 * */
+	public static void main(String args []){
+		
+		System.out.println(2^3^3^1^2);
+		System.out.println(2<<2);
+	}
+
+}
--- src/singlepointlist/SinglePointLinkedList.java
+++ src/singlepointlist/SinglePointLinkedList.java
@@ -4,14 +4,26 @@ public class SinglePointLinkedList {
 	
 	public int size  = 0;
 	
+	public Element headPoint;
 	
     class Element{
-    	String name;
+    	public Object value = null;
     	Element next;
     }
     
     public void add(Element mElement){
+    	size++;
+    	if(headPoint==null){
+    		headPoint = new Element();
+    		headPoint .next = null;
+    	}
+    	Element mElementTemp = new Element();
+    	mElementTemp.value = mElement;
     	
+    	
+    	//
+    	mElementTemp.next = headPoint.next;
+    	headPoint.next = mElementTemp;
     }
     
     public void remove(){
